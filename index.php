1998afb973b24a4fe808d017c67661a087445325
blaugrau3001 <86774829+blaugrau3001@users.noreply.github.com>
2023-04-01 11:57:03 +0200

    Initial commit

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
 <!DOCTYPE html>
 <head>
+
     <title>hello</title>
+
 </head>
+
 <body>
+
     <h1>WHATSSS UPPP</h1>
+
+    ez
+
+    <?php
+        echo "Hello"
+    ?>
+
+    <img src="./cdn/biden.jpg"
+
 </body>
